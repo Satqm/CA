cace02ccd53afd2ae49368b65c06807e160826e4
Satqm <satqm2991@gmail.com>
2026-01-01 18:12:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,39 @@
-// Firebase Configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBVUi-vAyvE-g_iK4W9zbTTa5Ps8MwZEtg",
-    authDomain: "ca-final-537aa.firebaseapp.com",
-    projectId: "ca-final-537aa",
-    storageBucket: "ca-final-537aa.firebasestorage.app",
-    messagingSenderId: "156489737512",
-    appId: "1:156489737512:web:f8b06ec7cba71ddfe99c1d"
-};
+const CACHE_NAME = 'ca-final-tracker-v2';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-
-// Enable offline persistence
-db.enablePersistence().catch((err) => {
-    console.log("Firestore persistence error: ", err.code);
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-// Export if using modules
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { auth, db };
-}
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
